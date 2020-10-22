f8c606868c602dd81e8aa966d5bc6f880b0cceda
shinybender <dominikselwa@gmail.com>
2020-10-22 09:26:22 +0200

    Added fake local_settings file

---
+++ make_shopping_easier/local_settings.py.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'HOST': 'localhost',
+        'PASSWORD': 'your_db_password',
+        'USER': 'your_db_user',
+    }
+}
+
+SECRET_KEY = 'your_secret_key'
